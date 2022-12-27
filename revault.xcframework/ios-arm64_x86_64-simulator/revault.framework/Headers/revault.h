f6affebec5d52ec88de440f33643ef755bfe3e3f
nunocnh <51781760+nunocnh@users.noreply.github.com>
2022-12-27 11:20:15 +0000

    Add files via upload

---
+++ revault.xcframework/ios-arm64_x86_64-simulator/revault.framework/Headers/revault.h
@@ -0,0 +1,18 @@
+//
+//  revault.h
+//  revault
+//
+//  Created by ITSector on 18/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for revault.
+FOUNDATION_EXPORT double revaultVersionNumber;
+
+//! Project version string for revault.
+FOUNDATION_EXPORT const unsigned char revaultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <revault/PublicHeader.h>
+
+
